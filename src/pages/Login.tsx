c0be4d010dde900843c7d067c50d9f51a7d10450
krishnatiwari005 <krishna.08072005@gmail.com>
2026-04-09 02:20:01 +0530

    solve deployement error

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { supabase } from '../lib/supabase';
 import { Shield, Eye, EyeOff, Loader2 } from 'lucide-react';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 
 
 export const Login = () => {
